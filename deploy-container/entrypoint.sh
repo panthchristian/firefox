26b588162750261f8962dd245a629f04e36d263b
Vansh Shah <65898155+panthchristian@users.noreply.github.com>
2021-05-11 11:53:25 +0530

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -6,4 +6,4 @@ mkdir -p $START_DIR
 
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
 
-docker run --env PORT=8888 -it -p 8888:8888 linuxserver/firefox:latest
+docker run --env PORT=8080 -it -p 8080:8080 linuxserver/firefox:latest
